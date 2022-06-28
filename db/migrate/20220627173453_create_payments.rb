83b71ab7e717b7c17dd8ce391f588651b849b653
imubarak234 <imubarak2424@gmail.com>
2022-06-28 17:09:13 +0100

    complete migrations

--- db/migrate/20220627173453_create_payments.rb
+++ db/migrate/20220627173453_create_payments.rb
@@ -3,7 +3,8 @@ class CreatePayments < ActiveRecord::Migration[7.0]
     create_table :payments do |t|
       t.float :amount
       t.date :date
-
+      t.references :user, foreign_key: true
+      t.references :account_detail, foreign_key: true 
       t.timestamps
     end
   end

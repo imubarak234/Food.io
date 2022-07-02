63d3a9506e4fc3b1907743bb183c04bf2a816bfd
imubarak234 <imubarak2424@gmail.com>
2022-07-02 15:43:21 +0100

    devise migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   has_many :account_details
   has_many :payments
   has_many :foods 
